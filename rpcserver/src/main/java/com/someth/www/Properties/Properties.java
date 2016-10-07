ae3fc61c48a9b8d61e54f9deeb2120a13a474d59
zqhZY <zqingheng@163.com>
2016-10-07 01:17:49 -0700

    add Properties file

---
+++ rpcserver/src/main/java/com/someth/www/Properties/Properties.java
@@ -0,0 +1,15 @@
+package com.zqh.www.Properties;
+
+/**
+ * Created by asus-pc on 2016/10/6.
+ */
+public class Properties {
+
+    //conf filename
+    public static final String RPC_SERVER_CONF_NAME = "rpc-server.properties";
+
+    //conf properties name
+    public static final String RPC_SERVER_NAME = "server.name";
+    public static final String RPC_SERVER_HOST = "server.host";
+    public static final String RPC_SERVER_PORT = "server.port";
+}
